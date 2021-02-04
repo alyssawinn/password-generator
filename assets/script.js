b643c1f251ae8ef6a4071a198cbed6bfc2743a84
Alyssa Winn <alyssawinn2@hotmail.com>
2021-02-03 18:20:57 -0700

    initial setup

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+// Assignment code here
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  console.log("hi");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
