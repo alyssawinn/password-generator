b04f68efcdfc9a388bd0d73b0347c7bdd0b32fe5
Alyssa Winn <alyssawinn2@hotmail.com>
2021-02-06 16:14:27 -0700

    add validation and arrays

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,69 @@
 // Assignment code here
+function createPassword() {
+  var useCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+  "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+  " ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"];
 
+  for (var i=0; i < passwordAttributes.length; i++) {
+    var randomIndex = [Math.floor(Math.random() * useCharacters.length)];
+    console.log(useCharacters[randomIndex]);
+  }
+}
+
+function validatePassword() {
+  var includeLowercase = window.confirm("Should your password contain lowercase letters?");
+  var includeUppercase = window.confirm("Should your password contain uppercase letters?");
+  var includeNumeric = window.confirm("Should your password contain numbers?");
+  var includeSpecial = window.confirm("Should your password contain special characters?");
+
+  if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+    window.alert("At least one character type must be selected.");
+    validatePassword();
+  } else {
+      if (includeLowercase) {
+        passwordAttributes.lowercaseChar = true;
+      }
+      if (includeUppercase) {
+        passwordAttributes.uppercaseChar = true;
+      }
+      if (includeNumeric) {
+        passwordAttributes.numericChar = true;
+      }
+      if (includeSpecial) {
+        passwordAttributes.specialChar = true;
+      }
+      createPassword();
+    }
+}
+
+function generatePassword() {
+  var passwordLength = window.prompt("How many characters do you want your password to be?");
+  passwordLength = parseInt(passwordLength);
+    if (Number.isNaN(passwordLength)) {
+      window.alert("Please enter a numeric value.");
+      generatePassword();
+    } else {
+        if (passwordLength < 8 || passwordLength > 128) {
+          window.alert("Password must be greater than 8 characters, but less than 129.");
+          generatePassword();
+        } else {
+          passwordAttributes.length = passwordLength;
+          validatePassword();
+        }
+      }
+}
+
+var passwordAttributes = [
+  {
+    length: 0,
+    lowercaseChar: false,
+    uppercaseChar: false,
+    specialChar: false,
+    numericChar: false,
+    passwordChar: ""
+  }
+]
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
